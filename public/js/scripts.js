eb9d5b1a55aa7757fd813b35d3c25965ca00d070
Spike <spike01@gmail.com>
2015-12-05 21:16:25 +0000

    Neaten up monster require function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,7 @@
 window.test_geo = null;
 
-require(["esri/map",
+require([
+    "esri/map",
     "esri/layers/FeatureLayer",
     "esri/dijit/LocateButton",
     "esri/toolbars/draw",
@@ -14,71 +15,76 @@ require(["esri/map",
 
     "dijit/layout/BorderContainer", "dijit/layout/ContentPane",
     "dijit/form/Button", "dijit/WidgetSet",
-    "dojo/domReady!"], function(Map, FeatureLayer, LocateButton, Draw, Graphic,
-      SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol, registry) {
-      var map = new Map("map", {
-        zoom: 15,
-        basemap: "topo"
-      });
+    "dojo/domReady!"], 
+    everythingElse)
+
+function everythingElse(Map, FeatureLayer, LocateButton, Draw, Graphic,
+                        SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol, 
+                        registry) {
+
+  var map = new Map("map", {
+    zoom: 15,
+    basemap: "topo"
+  });
 
 
-      navigator.geolocation.getCurrentPosition(function(position) { map.centerAndZoom([position.coords.longitude, position.coords.latitude], 15) });
+  navigator.geolocation.getCurrentPosition(function(position) { map.centerAndZoom([position.coords.longitude, position.coords.latitude], 15) });
 
-      map.on("load", createToolbar);
-      map.on("load", drawData);
+  map.on("load", createToolbar);
+  map.on("load", drawData);
 
-      navigator.geolocation.getCurrentPosition(function(position) {
-        map.centerAndZoom([position.coords.longitude, position.coords.latitude], 15)
-      });
+  navigator.geolocation.getCurrentPosition(function(position) {
+    map.centerAndZoom([position.coords.longitude, position.coords.latitude], 15)
+  });
 
-      geoLocate = new LocateButton({
-        map: map,
-        scale: 12000
-      }, "LocateButton");
-      geoLocate.startup();
+  geoLocate = new LocateButton({
+    map: map,
+    scale: 12000
+  }, "LocateButton");
+  geoLocate.startup();
 
-      drawBtn = $("#draw");
-      drawBtn.on("click", activateTool);
+  drawBtn = $("#draw");
+  drawBtn.on("click", activateTool);
 
-      function activateTool() {
-        drawBtn.hide();
-        toolbar.activate(Draw["FREEHAND_POLYLINE"]);
-        map.hideZoomSlider();
-      }
+  function activateTool() {
+    drawBtn.hide();
+    toolbar.activate(Draw["FREEHAND_POLYLINE"]);
+    map.hideZoomSlider();
+  }
 
-      function createToolbar(themap) {
-        toolbar = new Draw(map);
-        toolbar.on("draw-end", addAndSend);
-      }
+  function createToolbar(themap) {
+    toolbar = new Draw(map);
+    toolbar.on("draw-end", addAndSend);
+  }
 
-      function addAndSend(evt) {
-        addToMap(evt);
-        $.post("/save", JSON.stringify(evt.geometry), function(response) {
-          $("#share").attr("value", "http://techcrunch-one-direction.com/" + JSON.parse(response).key);
-          $("#link").modal();
-          new ShareButton({
-            url: JSON.parse(response).key
-          })
-        });
-      }
-
-      function addToMap(evt) {
-        var symbol;
-        toolbar.deactivate();
-        map.showZoomSlider();
-        symbol = new SimpleLineSymbol();
-
-        var graphic = new Graphic(evt.geometry, symbol);
-        map.graphics.add(graphic);
-      }
-
-      function drawData() {
-        if(window.geo) {
-          var symbol;
-          symbol = new SimpleLineSymbol();
-          var graphic = new Graphic({geometry: geo}, symbol);
-          graphic.symbol = new SimpleLineSymbol();
-          map.graphics.add(graphic)
-        }
-      }
+  function addAndSend(evt) {
+    addToMap(evt);
+    $.post("/save", JSON.stringify(evt.geometry), function(response) {
+      $("#share").attr("value", "http://techcrunch-one-direction.com/" + JSON.parse(response).key);
+      $("#link").modal();
+      new ShareButton({
+        url: JSON.parse(response).key
+      })
     });
+  }
+
+  function addToMap(evt) {
+    var symbol;
+    toolbar.deactivate();
+    map.showZoomSlider();
+    symbol = new SimpleLineSymbol();
+
+    var graphic = new Graphic(evt.geometry, symbol);
+    map.graphics.add(graphic);
+  }
+
+  function drawData() {
+    if(window.geo) {
+      var symbol;
+      symbol = new SimpleLineSymbol();
+      var graphic = new Graphic({geometry: geo}, symbol);
+      graphic.symbol = new SimpleLineSymbol();
+      map.graphics.add(graphic);
+    }
+  }
+};
